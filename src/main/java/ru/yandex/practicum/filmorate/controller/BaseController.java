6238150fc720fc572f3ad11a5a84e1b76df74c7c
Александра <aleksandra-palagina@mail.ru>
2023-12-28 20:25:33 +0300

    <ТЗ9>

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/BaseController.java
@@ -0,0 +1,37 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import ru.yandex.practicum.filmorate.model.BaseUnit;
+
+import javax.validation.ValidationException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class BaseController <T extends BaseUnit>{
+
+    private Map<Long, T> storage = new HashMap<>();
+    private long generateId = 0L;
+
+    public T create(T data){
+        validate(data);
+        data.setId(++generateId);
+        storage.put(data.getId(), data);
+        return data;
+    }
+
+    public T update(T data){
+        if(!storage.containsKey(data.getId())){
+            throw new ValidationException("Не найден айди " + data.getId());
+        }
+        validate(data);
+        storage.put(data.getId(), data);
+        return data;
+    }
+
+    public List<T> getAll(){
+        return new ArrayList<>(storage.values());
+    }
+
+    abstract void validate(T data);
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@SuperBuilder
+public class User extends BaseUnit{
+
+    private String name;
+
+    @NotEmpty
+    @Pattern(regexp = "^\\S+$")
+    private String login;
+
+    @NotEmpty
+    @Email(message = "неверно указан email")
+    private String email;
+
+    @Past
+    private LocalDate birthday;
+}
