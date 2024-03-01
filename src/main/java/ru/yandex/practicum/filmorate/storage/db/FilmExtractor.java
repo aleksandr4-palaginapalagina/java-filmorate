2640f992276b67ba09524af8d924d22ec0e81db2
Александра <aleksandra-palagina@mail.ru>
2024-03-01 19:06:17 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/FilmExtractor.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/FilmExtractor.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.storage.db;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.filmorate.model.BaseUnit;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
